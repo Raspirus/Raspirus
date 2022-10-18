1d3fff4f10e1bac4a0b7113d74bedb86d1f004c7
Benji <50681275+Benji377@users.noreply.github.com>
2022-10-18 21:42:43 +0200

    Merged two classes to one

--- class/FileScanner.cpp
+++ class/FileScanner.cpp
@@ -1,6 +1,7 @@
 #include <iostream> 
 #include <vector>
-#include "FileRetriever.cpp"
+#include <filesystem>
+#include "File.cpp"
 
 using namespace std; 
 /*
@@ -12,12 +13,22 @@ using namespace std;
 class FileScanner {
     private:
         vector<File> unscannedList, cleanFiles, dirtyFiles;
+        string path;
 
     public:
         FileScanner(string folder_path) {
-            FileRetriever fr = FileRetriever(folder_path);
-            this->unscannedList = fr.getFileList();
+            this->path = folder_path;
+            listFiles();
         }
+
+        void listFiles() {
+            // Algorithm to put all files at a destination into an array
+            for (const auto & entry : filesystem::directory_iterator(this->path)) {
+                this->unscannedList.push_back((File(entry.path().string())));
+                // Should list all files inside directory and sub-directory
+                std::cout << entry.path() << std::endl;
+            }
+        } 
 
         void scanFiles() {
             // Compare each Hash of a file with the hash in a list
