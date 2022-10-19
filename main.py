d44537b2a0c1fac7134d39d3f8830de7d3d5e2af
Benji <50681275+Benji377@users.noreply.github.com>
2022-10-19 18:25:16 +0200

    Extended main and added testing functions

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from backend.FileScanner import FileScanner
+from backend.HashAPI import HashAPI
 
-
+# This function isn't finished yet and used for testing purposes, will then be implemented later
 def main():
-    path_to_check = "C:/Users/benbe/Documents/Coding/WebProjects"
-    path_to_signatures = "C:/Users/benbe/Documents/School/MaturaProject/VirusShare_00000.md5"
+    print("#########################################################################")
+    print("# Insert a directory to scan and the scanner will start automatically!  #")
+    print("# Path syntax: 'C:/Users/someone/Documents/ToScan'                      #")
+    print("# REMEMBER! No '/' at the end and no '                                  #")
+    print("#########################################################################")
+    # Example: C:/Users/benbe/Documents/Coding/WebProjects
+    path_to_check = str(input("Enter path: "))
+    print("")
+    path_to_signatures = "C:/Users/benbe/Documents/School/MaturaProject/Github/backend/BigHash.db"
     fs = FileScanner(path_to_check, path_to_signatures)
     fs.start_scanner()
 
+def updater():
+    # Path to directory with md5 files/file extension, path to the bighash file
+    hapi = HashAPI("backend/SignatureLists/*md5", "backend/BigHash.db")
+    h_list = hapi.get_hash()
+    print("Length: " + str(len(h_list)))
 
-main()
+
+updater()
