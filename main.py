c7c42332697490bb0c6b3e00dbe82fd1b0432fca
Benji <50681275+Benji377@users.noreply.github.com>
2022-10-19 21:34:10 +0200

    Fixing path issues

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from backend.FileScanner import FileScanner
 from backend.HashAPI import HashAPI
 
+signature_path = "backend/BigHash.db"
+signature_lists_path = "backend/SignatureLists/*md5"
+
 # This function isn't finished yet and used for testing purposes, will then be implemented later
 def main():
     print("#########################################################################")
@@ -8,27 +11,25 @@ def main():
     print("# Path syntax: 'C:/Users/someone/Documents/ToScan'                      #")
     print("# REMEMBER! No '/' at the end and no '                                  #")
     print("#########################################################################")
-    # Example: C:/Users/benbe/Documents/Coding/WebProjects
     path_to_check = str(input("Enter path: "))
     print("")
-    path_to_signatures = "C:/Users/benbe/Documents/School/MaturaProject/Github/backend/BigHash.db"
-    fs = FileScanner(path_to_check, path_to_signatures)
+    fs = FileScanner(path_to_check, signature_path)
     fs.start_scanner()
 
 def updater():
     # Path to directory with md5 files/file extension, path to the bighash file
     # 38797306 Hashes
-    hapi = HashAPI("backend/SignatureLists/*md5", "backend/BigHash.db")
+    hapi = HashAPI(signature_lists_path, signature_path)
     hapi.update_bighash()
     h_list = hapi.get_hash()
     print("Length: " + str(len(h_list)))
 
 def downloader():
-    hapi = HashAPI("backend/SignatureLists/*md5", "backend/BigHash.db")
+    hapi = HashAPI(signature_lists_path, signature_path)
     hapi.download_new_signatures("backend/SignatureLists")
 
 def more_info():
-    hapi = HashAPI("backend/SignatureLists/*md5", "backend/BigHash.db")
+    hapi = HashAPI(signature_lists_path, signature_path)
     hapi.get_hash_info("backend/temp/test.json", "ecb9cf121345c404495d99c737c7d3bf")
 
-updater()
+main()
