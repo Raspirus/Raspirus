586ba85a667c2d759ec8d9c576098927635ed277
Benji <50681275+Benji377@users.noreply.github.com>
2022-10-23 17:39:27 +0200

    Experimenting with multiple windows

--- frontend/main.py
+++ frontend/main.py
@@ -14,12 +14,56 @@ QML_IMPORT_MAJOR_VERSION = 1
 
 # Important:
 # Material Theme: https://doc.qt.io/qt-6/qtquickcontrols2-material.html
+app = QGuiApplication(sys.argv)
+QQuickStyle.setStyle("Material")
+engine = QQmlApplicationEngine()
+
+
+@QmlElement
+class Bridge(QObject):
+
+    @Slot(str, result=str)
+    def getColor(self, s):
+        if s.lower() == "red":
+            return "#ef9a9a"
+        elif s.lower() == "green":
+            return "#a5d6a7"
+        elif s.lower() == "blue":
+            return "#90caf9"
+        else:
+            return "white"
+
+    @Slot(float, result=int)
+    def getSize(self, s):
+        size = int(s * 34)
+        if size <= 0:
+            return 1
+        else:
+            return size
+
+    @Slot(str, result=bool)
+    def getItalic(self, s):
+        if s.lower() == "italic":
+            return True
+        else:
+            return False
+
+    @Slot(str, result=bool)
+    def getBold(self, s):
+        if s.lower() == "bold":
+            return True
+        else:
+            return False
+
+    @Slot(str, result=bool)
+    def getUnderline(self, s):
+        if s.lower() == "underline":
+            return True
+        else:
+            return False
+
 
 if __name__ == '__main__':
-    app = QGuiApplication(sys.argv)
-    QQuickStyle.setStyle("Material")
-    engine = QQmlApplicationEngine()
-
     # Get the path of the current directory, and then add the name
     # of the QML file, to load it.
     qml_file = Path(__file__).parent / 'views/main.qml'
@@ -28,4 +72,6 @@ if __name__ == '__main__':
     if not engine.rootObjects():
         sys.exit(-1)
 
+    # Warning! Closing the settings window after creating it doesn't close
+    # the mainwindow, that just remains hidden
     sys.exit(app.exec())
