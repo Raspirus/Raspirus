faac7a6ed0c64e5c7595820b9dbac3e425752f5d
Benji <50681275+Benji377@users.noreply.github.com>
2022-10-24 18:20:36 +0200

    Downgrading to PyQt5
    The Raspberry can't use PySide6, so I had to downgrade to PyQt5

--- frontend/main.py
+++ frontend/main.py
@@ -1,77 +1,41 @@
 import sys
-from pathlib import Path
+import os
 
-from PySide6.QtCore import QObject, Slot
-from PySide6.QtGui import QGuiApplication
-from PySide6.QtQml import QQmlApplicationEngine, QmlElement
-from PySide6.QtQuickControls2 import QQuickStyle
+from PyQt5.QtCore import QUrl, QObject
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtQml import QQmlApplicationEngine
 
-# To be used on the @QmlElement decorator
-# (QML_IMPORT_MINOR_VERSION is optional)
-QML_IMPORT_NAME = "io.qt.textproperties"
-QML_IMPORT_MAJOR_VERSION = 1
+# Helpful docu: https://github.com/seanwu1105/pyqt5-qtquick2-example
 
+# Set the QtQuick Style
+# Acceptable values: Default, Fusion, Imagine, Material, Universal.
+os.environ['QT_QUICK_CONTROLS_STYLE'] = "Material"
 
-# Important:
-# Material Theme: https://doc.qt.io/qt-6/qtquickcontrols2-material.html
-app = QGuiApplication(sys.argv)
-QQuickStyle.setStyle("Material")
+# Create an instance of the application
+# QApplication MUST be declared in global scope to avoid segmentation fault
+app = QApplication(sys.argv)
+
+# Create QML engine
 engine = QQmlApplicationEngine()
 
+# Load the qml file into the engine
+engine.load(QUrl('views/main.qml'))
 
-@QmlElement
-class Bridge(QObject):
+# Qml file error handling
+if not engine.rootObjects():
+    sys.exit(-1)
 
-    @Slot(str, result=str)
-    def getColor(self, s):
-        if s.lower() == "red":
-            return "#ef9a9a"
-        elif s.lower() == "green":
-            return "#a5d6a7"
-        elif s.lower() == "blue":
-            return "#90caf9"
-        else:
-            return "white"
+# Send QT_QUICK_CONTROLS_STYLE to main qml (only for demonstration)
+# For more details and other methods to communicate between Qml and Python:
+#   http://doc.qt.io/archives/qt-4.8/qtbinding.html
+#qtquick2Themes = engine.rootObjects()[0].findChild(
+#    QObject,
+#    'qtquick2Themes'
+#)
+#qtquick2Themes.setProperty('text', os.environ['QT_QUICK_CONTROLS_STYLE'])
 
-    @Slot(float, result=int)
-    def getSize(self, s):
-        size = int(s * 34)
-        if size <= 0:
-            return 1
-        else:
-            return size
+# engine.quit.connect(app.quit)
+# Unnecessary,
+# since QQmlEngine.quit has already connect to QCoreApplication.quit
 
-    @Slot(str, result=bool)
-    def getItalic(self, s):
-        if s.lower() == "italic":
-            return True
-        else:
-            return False
-
-    @Slot(str, result=bool)
-    def getBold(self, s):
-        if s.lower() == "bold":
-            return True
-        else:
-            return False
-
-    @Slot(str, result=bool)
-    def getUnderline(self, s):
-        if s.lower() == "underline":
-            return True
-        else:
-            return False
-
-
-if __name__ == '__main__':
-    # Get the path of the current directory, and then add the name
-    # of the QML file, to load it.
-    qml_file = Path(__file__).parent / 'views/main.qml'
-    engine.load(qml_file)
-
-    if not engine.rootObjects():
-        sys.exit(-1)
-
-    # Warning! Closing the settings window after creating it doesn't close
-    # the mainwindow, that just remains hidden
-    sys.exit(app.exec())
+sys.exit(app.exec_())
