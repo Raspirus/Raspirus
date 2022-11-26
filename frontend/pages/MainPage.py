040163bdd8661d0eb24ca116193d9b5f6ae8b93e
Benji <50681275+Benji377@users.noreply.github.com>
2022-11-26 16:27:59 +0100

    Switching from Listbox to Combobox

--- frontend/pages/MainPage.py
+++ frontend/pages/MainPage.py
@@ -1,11 +1,13 @@
 import tkinter as tk
+from tkinter import ttk
 from Raspirus.frontend.popups.SingleButtonDialog import SingleButtonDialog
 from Raspirus.frontend.utility import *  # For colors and fonts
+import os
 
 
 class MainPage(tk.Frame):
     title_label: tk.Label
-    drive_selector: tk.Listbox
+    drive_selector: ttk.Combobox
     start_btn: tk.Button
     info_btn: tk.Button
     settings_btn: tk.Button
@@ -18,13 +20,13 @@ class MainPage(tk.Frame):
         self.title_label.place(x=140, y=60, width=510, height=120)
 
         # highlightbackground / highlightcolor sets a border to the component
-        self.drive_selector = tk.Listbox(self, highlightbackground=PRIMARY_COLOR, highlightcolor=PRIMARY_COLOR,
-                                         highlightthickness=2, bg=TEXT_COLOR)
+        self.drive_selector = ttk.Combobox(self, font=NORMAL_TEXT_FONT, state='readonly')
         self.drive_selector.place(x=90, y=215, width=620, height=48)
+        self.load_drive_list()
 
         self.start_btn = tk.Button(self, text="START", font=BUTTON_TEXT_FONT,
                                    fg=BACKGROUND_COLOR, bg=PRIMARY_COLOR)
-        self.start_btn.config(command=lambda: self.start_scanner())
+        self.start_btn.config(command=lambda: self.start_scanner(controller=controller))
         self.start_btn.place(x=185, y=315, width=170, height=50)
 
         self.info_btn = tk.Button(self, text="INFO", font=BUTTON_TEXT_FONT,
@@ -37,13 +39,22 @@ class MainPage(tk.Frame):
         self.settings_btn.config(command=lambda: controller.show_frame(controller.pages[1]))
         self.settings_btn.place(x=670, y=15, width=110, height=40)
 
-    def start_scanner(self):
-        no_drive_message = "Before starting the scanner you need to specify which " \
+    def start_scanner(self, controller):
+        # Checks if the given string is empty
+        if len(self.drive_selector.get()) <= 0 or len(str(self.drive_selector.get()).strip()) <= 0:
+            no_drive_message = "Before starting the scanner you need to specify which " \
                            "harddrive or USB you want to scan by selecting " \
                            "it from the dropdown menu"
-        dialog = SingleButtonDialog(title="No Drive", parent=self,
-                                    message=no_drive_message, mode="error")
-        dialog.tkraise()
+            dialog = SingleButtonDialog(title="No Drive", parent=self,
+                                        message=no_drive_message, mode="error")
+            dialog.tkraise()
+        else:
+            controller.show_frame(controller.pages[2])
 
-        # controller.show_frame(controller.pages[2])
+    def load_drive_list(self):
+        # Linux options: https://stackoverflow.com/a/8265634
+        # Windows: https://stackoverflow.com/a/8110666
+        test_list = ["Test string numbero", "Some more teststs", " With some space front", "Short",
+                     " Very very very very very vfery hags hasgwqgdi iqdidnob iqbiq LONG", "      "]
+        self.drive_selector["values"] = test_list
 
