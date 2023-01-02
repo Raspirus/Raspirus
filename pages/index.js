31c42718498d2d4efa6bb18e6ffd20f62f377457
Benji <50681275+Benji377@users.noreply.github.com>
2023-01-02 11:48:35 +0100

    Removed unused dependencies

--- pages/index.js
+++ pages/index.js
@@ -5,21 +5,25 @@ import { invoke } from "@tauri-apps/api/tauri"
 
 export default function Home() {
   const router = useRouter();
-  const [value, setValue] = useState("drive_0");
-  let drives_list = null;
+  const [value, setValue] = useState("");
+  let drives = null;
 
   if (typeof window !== 'undefined') {
     invoke('list_usb_drives')
-    .then((message) => drives_list = message)
-    .catch(console.error);
+      .then(console.log)
+      .then((message) => drives = message)
+      .catch(console.error);
 
-    console.log("Drives: " + drives_list);
+    console.log("Drives: " + drives);
   }
 
   async function startScanner() {
     router.push('/loading');
     console.log("Value = " + value);
     let dirty_array = null;
+    if (value == "") {
+      return;
+    }
     let scanning_path = value;
     let should_update = false;
     let db_location = "";
@@ -28,12 +32,12 @@ export default function Home() {
       await invoke('start_scanner', { path: scanning_path, update: should_update, dbfile: db_location })
         .then((message) => dirty_array = message)
         .catch((console.error));
-        console.log(dirty_array);
-        if (dirty_array != null && dirty_array.length > 0) {
-          router.push('/infected');
-        } else {
-          router.push('/clean');
-        }
+      console.log(dirty_array);
+      if (dirty_array != null && dirty_array.length > 0) {
+        router.push('/infected');
+      } else {
+        router.push('/clean');
+      }
     } else {
       console.error("Nextjs not in client mode!");
     }
@@ -60,7 +64,7 @@ export default function Home() {
           <button onClick={openSettings} type="button" className="inline-block px-6 py-2 border-2 m-5 border-mainred text-mainred 
         font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 
         focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
-          <i className="pr-1 fa fa-gear"></i> SETTINGS</button>
+            <i className="pr-1 fa fa-gear"></i> SETTINGS</button>
         </div>
 
 
@@ -69,15 +73,23 @@ export default function Home() {
             <div className="flex justify-center items-center h-full">
               <div className="w-full">
                 <h1 className="font-bold leading-tight text-8xl mt-0 mb-2 text-mainred">RASPIRUS</h1>
-                <select placeholder='Select drive' value={value} onChange={(e) => {setValue(e.target.value);}} className="
+                {drives && drives.length > 0 ? (
+                <select placeholder='Select drive' value={value} onChange={(e) => { setValue(e.target.value); }} className="
                         px-3 py-1.5 text-base font-normal text-gray-700 bg-white w-9/12
                         border border-solid border-maingreen-light rounded transition ease-in-out
                         focus:text-gray-700 focus:bg-white focus:border-maingreen focus:outline-none">
-                  <option value=".">This will scan the current folder</option>
-                  <option value="/">This will scan the / folder</option>
-                  <option value="C:/Users/benbe/Documents/School/Deutsch">Windows only</option>
-                  <option value="/home/pi/FTP/">Linux PI only</option>
+                      <option value="" disabled>Select drive</option>
+                  {drives && drives.map((drive) => (
+                    <option value={drive}>{drive}</option>
+                  ))}
                 </select>
+                ) : (
+                  <div className="
+                  m-auto px-3 py-1.5 text-base font-normal text-gray-700 bg-white w-9/12
+                  border border-solid border-maingreen-light rounded transition ease-in-out
+                  focus:text-gray-700 focus:bg-white focus:border-maingreen focus:outline-none">
+                    No drives found. Try refreshing this page later</div>
+                )}
                 <div className="mt-5">
                   <button onClick={startScanner} type="button" className="mr-2 inline-block px-7 py-3 bg-mainred text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-mainred-dark hover:shadow-lg focus:bg-mainred-dark focus:shadow-lg focus:outline-none focus:ring-0 active:mainred-dark active:shadow-lg transition duration-150 ease-in-out">START</button>
                   <button onClick={openInfo} type="button" className="ml-2 inline-block px-7 py-3 border-2 border-mainred text-mainred font-medium text-sm leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">INFO</button>
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,9 @@
     windows_subsystem = "windows"
 )]
 
-use std::{path::Path, time, fs};
-
+use std::{path::Path, time, fs, thread, env, path::PathBuf};
 use backend::file_scanner;
+use winapi::um::fileapi;
 use log::{error, info, warn};
 
 mod backend;
@@ -72,8 +72,7 @@ fn start_scanner(path: String, update: bool, dbfile: Option<String>) -> Result<(
     } else {
         info!("Skipped update");
     }
-
-    fs.search_files();
+    thread::spawn(move || fs.search_files()).join().unwrap();
     Ok(())
 }
 
@@ -84,20 +83,67 @@ fn start_scanner(path: String, update: bool, dbfile: Option<String>) -> Result<(
 /// A `Result` object containing a vector of strings representing the paths to the USB drives, or an `Err` with an error message if an error occurred.
 #[tauri::command]
 fn list_usb_drives() -> Result<Vec<String>, String> {
+
+    match pretty_env_logger::try_init() {
+        Ok(()) => {
+            info!("Logger initialized!");
+        }
+        Err(err) => {
+            warn!("Failed initializing logger: {err}");
+        }
+    }
+
     let mut usb_drives = Vec::new();
 
-    let entries = match fs::read_dir("/dev") {
-        Ok(entries) => entries,
-        Err(err) => {
-            return Err(err.to_string());
-        }
-    };
+    if cfg!(target_os = "windows") {
+        info!("Trying to retrieve USB drives from Windows OS");
+        let system_drive = env::var_os("SystemDrive").unwrap_or_default();
+        let drives = env::split_paths(&system_drive);
+        let drivez = env::split_paths(&system_drive).collect::<Vec<_>>();
+        info!("Drives: {drivez:?}");
+        for drive in drives {
+            let drive_str = drive.to_str().unwrap();
+            info!("Drive found: {drive_str:?}");
+            let entries = match fs::read_dir(drive_str) {
+                Ok(entries) => entries,
+                Err(err) => {
+                    return Err(err.to_string());
+                }
+            };
 
-    for entry in entries {
-        let entry = entry.expect("I couldn't read something inside the directory");
-        let path = entry.path();
-        if path.starts_with("/dev/sd") || path.starts_with("/dev/mmc") {
-            usb_drives.push(path.to_string_lossy().into_owned());
+            for entry in entries {
+                info!("Entries: {entry:?}");
+                let entry = entry.expect("I couldn't read something inside the directory");
+                let path = entry.path();
+                if path.is_dir() && path.starts_with(drive_str) {
+                    let mut path_buf = PathBuf::new();
+                    path_buf.push(path);
+                    let drive_type = unsafe {
+                        let mut drive = path_buf.to_str().unwrap().encode_utf16().collect::<Vec<u16>>();
+                        drive.push(0);
+                        fileapi::GetDriveTypeW(drive.as_ptr())
+                    };
+                    if drive_type == winapi::um::winbase::DRIVE_REMOVABLE {
+                        usb_drives.push(path_buf.to_str().unwrap().to_owned());
+                    }
+                }
+            }
+        }
+    } else if cfg!(target_os = "linux") {
+        info!("Trying to retrieve USB drives from Linux OS");
+        let entries = match fs::read_dir("/dev") {
+            Ok(entries) => entries,
+            Err(err) => {
+                return Err(err.to_string());
+            }
+        };
+
+        for entry in entries {
+            let entry = entry.expect("I couldn't read something inside the directory");
+            let path = entry.path();
+            if path.starts_with("/dev/sd") || path.starts_with("/dev/mmc") {
+                usb_drives.push(path.to_string_lossy().into_owned());
+            }
         }
     }
 
