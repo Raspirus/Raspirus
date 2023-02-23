1bbdd34d65b6cb564d86fda6705cfaeadfc31af8
Benji <50681275+Benji377@users.noreply.github.com>
2023-02-23 15:27:41 +0100

    centering the image

--- pages/infected.js
+++ pages/infected.js
@@ -21,7 +21,7 @@ export default function Infected() {
                 <Head>
                     <title>Virus found!</title>
                 </Head>
-                <div className="flex items-center justify-center h-full flex-col">
+                <div className="flex items-center justify-center h-screen flex-col">
                     <h1 className="text-center mb-10 font-medium leading-tight text-5xl mt-0 text-mainred">Virus found!</h1>
                     <img src="/images/failure_image.png" alt="Failure" className="max-w-[30%]" />
                     <button onClick={backHome} type="button" className="inline-block px-6 py-2.5 m-10 bg-mainred text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-mainred-dark hover:shadow-lg focus:bg-mainred-dark focus:shadow-lg focus:outline-none focus:ring-0 active:bg-mainred-dark active:shadow-lg transition duration-150 ease-in-out">
