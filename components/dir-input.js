23257153b92322987e7d1c3981cd27b34867952a
Benji <50681275+Benji377@users.noreply.github.com>
2023-03-18 17:21:24 +0100

    Fixed #102

--- components/dir-input.js
+++ components/dir-input.js
@@ -5,7 +5,7 @@ export default function DirectoryInput({value}) {
             className="
                   m-auto px-3 py-1.5 text-base font-normal text-gray-700 bg-white inline-block w-full
                   border border-solid border-maingreen-light rounded transition ease-in-out mr-2
-                  focus:text-gray-700 focus:bg-white focus:border-maingreen focus:outline-none"
+                  focus:text-gray-700 focus:bg-white focus:border-maingreen focus:outline-none overflow-hidden max-w-lg max-h-9"
         >
             {value}
         </div>
