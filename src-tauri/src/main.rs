6080df0fe0d00a7f488e436e1f703815d4cb937b
GamingGuy003 <bjornfelixh@gmail.com>
2024-02-21 21:14:28 +0100

    Made code more neat

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -239,17 +239,13 @@ async fn list_usb_drives() -> Result<String, String> {
 // Creates the config from the GUI
 #[tauri::command]
 fn create_config(contents: Option<String>) -> Result<String, String> {
-    let config = match contents {
+    update_config(match contents {
         Some(contents) => serde_json::from_str(&contents).map_err(|err| err.to_string())?,
         None => get_config(),
-    };
+    }).map_err(|err| err.to_string())?;
 
-    update_config(config).map_err(|err| err.to_string())?;
-
-    let config_str = serde_json::to_string(&get_config())
-        .expect("Issue with transforming config to Serde string");
-
-    Ok(config_str)
+    serde_json::to_string(&get_config())
+        .map_err(|err| format!("Failed to convert config to json: {err}"))
 }
 
 #[tauri::command]
