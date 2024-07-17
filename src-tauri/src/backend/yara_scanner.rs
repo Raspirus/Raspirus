c7e879ba9e811f81294199c3a422a1d0882c6249
GamingGuy003 <bjornfelixh@gmail.com>
2024-07-17 21:26:03 +0200

    Fixed cargo toml, added initial yar deserializer

--- src-tauri/src/backend/yara_scanner.rs
+++ src-tauri/src/backend/yara_scanner.rs
@@ -1,11 +1,15 @@
+use std::{fs::File, path::PathBuf};
+
 use yara_x::Rules;
 
 pub struct YaraScanner {
-    pub rules: Rules, 
+    pub rules: Rules,
 }
 
 impl YaraScanner {
-    pub fn new(yar_path: PathBuf) -> Self {
-        let rules = Rules::deserialize_from();
+    pub fn new(yar_path: PathBuf) -> Result<Self, String> {
+        let reader = File::open(yar_path).map_err(|err| format!("Failed to load yar file: {}", err.to_string()))?;
+        let rules = Rules::deserialize_from(reader).map_err(|err| format!("Failed to deserialize yar file: {}", err.to_string()))?;
+        Ok(Self { rules })
     }
 }
